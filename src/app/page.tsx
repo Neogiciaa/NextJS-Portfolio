70eaf37fbeaf420ee553e8281a16ca689fbceb98
Neogiciaa <chevalier.stephen@hotmail.com>
2024-05-20 18:12:25 +0200

    Testing base structure 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,12 +12,12 @@ export default function Home() {
     return (
         <div className="min-h-screen flex flex-col items-center bg-black">
             <main className="w-full max-w-6xl p-8">
-                <div className="text-center mt-16 mb-16">
-                    <h1 className="text-4xl font-bold text-white">Stéphen Chevalier - Web Developer</h1>
+                <div className="text-center mt-16">
+                    <h1 className="text-4xl font-bold text-white mb-24">Stéphen Chevalier - Web Developer</h1>
                 </div>
 
                 <div className={`flex ${selectedSection ? 'flex-col md:flex-row' : 'flex-col items-center'} gap-8 items-start`}>
-                    <div className={`grid ${selectedSection ? 'grid-cols-1 md:grid-cols-1' : 'grid-cols-1 md:grid-cols-4'} gap-8 w-full md:w-${selectedSection ? '1/4' : 'full'} transition-all duration-300`}>
+                    <div className={`grid ${selectedSection ? 'grid-cols-1' : 'grid-cols-1 md:grid-cols-4'} gap-8 w-full md:w-${selectedSection ? '1/4' : 'full'} transition-all duration-300`}>
                         <div
                             onClick={() => handleClick('Qui suis-je ?')}
                             className={`bg-white p-6 rounded-lg border border-gray-300 flex flex-col items-center justify-center text-center cursor-pointer ${selectedSection === 'Qui suis-je ?' ? 'bg-gray-200' : ''}`}
@@ -48,28 +48,21 @@ export default function Home() {
                     </div>
 
                     {selectedSection && (
-                        <div className="flex-grow w-full md:w-3/4 bg-white p-6 rounded-lg border border-gray-300">
+                        <div className="flex-grow w-full md:w-3/4 bg-white p-6 rounded-lg border border-gray-300 mt-8 md:mt-0 md:ml-8">
                             {selectedSection === 'Qui suis-je ?' && (
                                 <div className="flex flex-col md:flex-row items-center md:items-start">
                                     <img src="/StephenChevalier.jpg" alt="Photo de Stéphen Chevalier" className="w-40 h-40 rounded-full md:mr-8 mb-4 md:mb-0"/>
                                     <div className="text-black">
                                         <p className="items-center">Hello moi c'est <strong>Stéphen</strong> !</p>
-                                        <br/>
-                                        <p>Développeur web passionné par la création de
-                                            solutions innovantes, mon objectif est de donner vie à des applications
-                                            web/mobiles performantes et intuitives qui répondent à vos rêves les plus
-                                            fous !</p>
-                                        <br/>
-                                        <br/>
-                                        <p>Blablabla</p>
-                                        <br/>
-                                        <br/>
-                                        <p>Blablabla</p>
+                                        <p className="my-4">Développeur web passionné par la création de solutions innovantes, mon objectif est de donner vie à des applications web/mobiles performantes et intuitives qui répondent à vos rêves les plus fous !</p>
+                                        <p className="my-4">Blablabla</p>
+                                        <p className="my-4">Blablabla</p>
                                     </div>
                                 </div>
                             )}
                             {selectedSection === 'Mon parcours' && (
                                 <div>
+                                    <h2 className="text-2xl font-semibold mb-4 text-black">Mon parcours</h2>
                                     <p className="text-black">Blablabla</p>
                                 </div>
                             )}
@@ -77,28 +70,23 @@ export default function Home() {
                                 <div>
                                     <div className="grid grid-cols-1 gap-4">
                                         <div className="bg-gray-100 p-4 rounded-lg">
-                                            <h3 className="text-xl font-semibold mb-2 text-black">Summoners War Siege Datas -</h3>
-                                            <p className="text-black">Blablabla <a
-                                                href="/projets/projet-1" className="text-blue-500 underline">Lire
-                                                plus</a></p>
+                                            <h3 className="text-xl font-semibold mb-2 text-black">Summoners War Siege Datas</h3>
+                                            <p className="text-black">Blablabla <a href="/projets/projet-1" className="text-blue-500 underline">Lire plus</a></p>
                                         </div>
                                         <div className="bg-gray-100 p-4 rounded-lg">
-                                            <h3 className="text-xl font-semibold mb-2 text-black">BornToTravel -</h3>
-                                            <p className="text-black">BornToTravel <a href="/projets/projet-2"
-                                                                                      className="text-blue-500 underline">Lire
-                                                plus</a></p>
+                                            <h3 className="text-xl font-semibold mb-2 text-black">BornToTravel</h3>
+                                            <p className="text-black">BornToTravel <a href="/projets/projet-2" className="text-blue-500 underline">Lire plus</a></p>
                                         </div>
                                         <div className="bg-gray-100 p-4 rounded-lg">
-                                            <h3 className="text-xl font-semibold mb-2 text-black">StockIO -</h3>
-                                            <p className="text-black">Blablabla <a href="/projets/projet-2"
-                                                                                   className="text-blue-500 underline">Lire
-                                                plus</a></p>
+                                            <h3 className="text-xl font-semibold mb-2 text-black">StockIO</h3>
+                                            <p className="text-black">Blablabla <a href="/projets/projet-2" className="text-blue-500 underline">Lire plus</a></p>
                                         </div>
                                     </div>
                                 </div>
                             )}
                             {selectedSection === 'Contactez-moi' && (
                                 <div>
+                                    <h2 className="text-2xl font-semibold mb-4 text-black">Contactez-moi</h2>
                                     <form className="text-black">
                                         <div className="mb-4">
                                             <label className="block text-black mb-2">Nom</label>
